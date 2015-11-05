5ce90378cb04ba8293a1b1ac245769f12807cc27
Mathias Dalheimer <md@gonium.net>
2015-11-05 15:12:51 +0100

    added CLI library for commandline parameter parsing.

--- src/github.com/gonium/gosdm630/cmd/sdm630_httpd/main.go
+++ src/github.com/gonium/gosdm630/cmd/sdm630_httpd/main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
-	"flag"
+	"github.com/codegangsta/cli"
 	"github.com/gonium/gosdm630"
+	"os"
 )
 
-var rtuDevice = flag.String("rtuDevice", "/dev/ttyUSB0", "Path to serial RTU device")
-var verbose = flag.Bool("verbose", false, "Enables extensive logging")
-
-func init() {
-	flag.Parse()
-}
-
 func main() {
-	var rc = make(sdm630.ReadingChannel)
-	// Read the SDM630 every 5 seconds
-	interval := 5
-	qe := sdm630.NewQueryEngine(*rtuDevice, interval, *verbose, rc)
-	go qe.Produce()
-	mc := sdm630.NewMeasurementCache(rc, interval)
-	go mc.ConsumeData()
-	sdm630.Run_httpd(mc)
+	app := cli.NewApp()
+	app.Name = "sdm630_httpd"
+	app.Usage = "SDM630 power measurements via HTTP."
+	app.Version = "0.1.0"
+	app.HideVersion = true
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "device, d",
+			Value: "/dev/ttyUSB0",
+			Usage: "path to serial RTU device",
+		},
+		cli.BoolFlag{
+			Name:  "verbose, v",
+			Usage: "print verbose messages",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		var rc = make(sdm630.ReadingChannel)
+		// Read the SDM630 every 5 seconds
+		interval := 5
+		qe := sdm630.NewQueryEngine(c.String("device"), interval,
+			c.Bool("verbose"), rc)
+		go qe.Produce()
+		mc := sdm630.NewMeasurementCache(rc, interval)
+		go mc.ConsumeData()
+		sdm630.Run_httpd(mc)
+	}
+
+	app.Run(os.Args)
 }
