375c7708ee1e0d21492dd2a7c79276a0b3d6a8eb
Mathias Dalheimer <md@gonium.net>
2017-02-06 16:51:10 +0100

    changed release tag

--- src/github.com/gonium/gosdm630/cmd/sdm630_httpd/main.go
+++ src/github.com/gonium/gosdm630/cmd/sdm630_httpd/main.go
@@ -14,7 +14,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "sdm630_httpd"
 	app.Usage = "SDM630 power measurements via HTTP."
-	app.Version = "0.2.0"
+	app.Version = "0.3.0"
 	app.HideVersion = true
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
--- src/github.com/gonium/gosdm630/cmd/sdm630_monitor/main.go
+++ src/github.com/gonium/gosdm630/cmd/sdm630_monitor/main.go
@@ -32,7 +32,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "sdm630_monitor"
 	app.Usage = "SDM630 monitor"
-	app.Version = "0.2.0"
+	app.Version = "0.3.0"
 	app.HideVersion = true
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
