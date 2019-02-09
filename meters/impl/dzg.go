626086e63ccd92f2518ca2dad91ab4f3ae136091
andig <cpuidle@gmx.de>
2019-02-09 16:20:58 +0100

    Change import/export

--- meters/impl/dzg.go
+++ meters/impl/dzg.go
@@ -21,32 +21,32 @@ func NewDZGProducer() Producer {
 	 * https://www.dzg.de/fileadmin/dzg/content/downloads/produkte-zaehler/dvh4013/Communication-Protocol_DVH4013.pdf
 	 */
 	ops := Opcodes{
-		// ImportActivePower: 0x0000, // 0x0000 - parameters
-		// ExportActivePower: 0x0002,
-		VoltageL1:     0x0004,
-		VoltageL2:     0x0006,
-		VoltageL3:     0x0008,
-		CurrentL1:     0x000A,
-		CurrentL2:     0x000C,
-		CurrentL3:     0x000E,
-		Cosphi:        0x0010,
-		Frequency:     0x0012,
-		Import:        0x4000, // 0x4000 - energy
-		ImportL1:      0x4020,
-		ImportL2:      0x4040,
-		ImportL3:      0x4060,
-		Export:        0x4100, // 0x.1.. - reverse
-		ExportL1:      0x4120,
-		ExportL2:      0x4140,
-		ExportL3:      0x4160,
-		ImportPower:   0x8000, // 0x8000 - demand(power)
-		ImportPowerL1: 0x8020,
-		ImportPowerL2: 0x8040,
-		ImportPowerL3: 0x8060,
-		ExportPower:   0x8100, // 0x.1.. - reverse
-		ExportPowerL1: 0x8120,
-		ExportPowerL2: 0x8140,
-		ExportPowerL3: 0x8160,
+		ImportPower: 0x0000, // 0x0000 - parameters
+		ExportPower: 0x0002,
+		VoltageL1:   0x0004,
+		VoltageL2:   0x0006,
+		VoltageL3:   0x0008,
+		CurrentL1:   0x000A,
+		CurrentL2:   0x000C,
+		CurrentL3:   0x000E,
+		Cosphi:      0x0010,
+		Frequency:   0x0012,
+		Import:      0x4000, // 0x4000 - energy
+		ImportL1:    0x4020,
+		ImportL2:    0x4040,
+		ImportL3:    0x4060,
+		Export:      0x4100, // 0x.1.. - reverse
+		ExportL1:    0x4120,
+		ExportL2:    0x4140,
+		ExportL3:    0x4160,
+		// ImportPower:   0x8000, // 0x8000 - demand(power)
+		// ImportPowerL1: 0x8020,
+		// ImportPowerL2: 0x8040,
+		// ImportPowerL3: 0x8060,
+		// ExportPower:   0x8100, // 0x.1.. - reverse
+		// ExportPowerL1: 0x8120,
+		// ExportPowerL2: 0x8140,
+		// ExportPowerL3: 0x8160,
 		// ImportPower:0x0014, // exception '2' (illegal data address)
 		// ExportPower:0x0016, // exception '2' (illegal data address)
 	}
@@ -96,11 +96,18 @@ func (p *DZGProducer) Produce() (res []Operation) {
 	}
 
 	for _, op := range []Measurement{
-		ImportPower, ImportPowerL1, ImportPowerL2, ImportPowerL3,
-		ExportPower, ExportPowerL1, ExportPowerL2, ExportPowerL3,
+		ImportPower, ExportPower,
 	} {
-		res = append(res, p.snip(op, 10000)) // factor 10000 = kW according to docs, but is W?
+		res = append(res, p.snip(op, 10)) // W
 	}
+
+	// these are "maximum" values, apparently retrieving "current" does not work
+	// for _, op := range []Measurement{
+	// 	ImportPower, ImportPowerL1, ImportPowerL2, ImportPowerL3,
+	// 	ExportPower, ExportPowerL1, ExportPowerL2, ExportPowerL3,
+	// } {
+	// 	res = append(res, p.snip(op, 10000)) // factor 10000 = kW according to docs, but is W?
+	// }
 
 	for _, op := range []Measurement{
 		Import, ImportL1, ImportL2, ImportL3,
