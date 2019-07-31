efd50e453d2bd81f7bef637652554d3cba940f93
andig <cpuidle@gmx.de>
2019-07-31 22:06:52 +0200

    Add inspect command

---
+++ cmd/inspect.go
@@ -0,0 +1,150 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/grid-x/modbus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/volkszaehler/mbmd/meters"
+
+	sunspec "github.com/andig/gosunspec"
+	bus "github.com/andig/gosunspec/modbus"
+	_ "github.com/andig/gosunspec/models" // import models
+	"github.com/andig/gosunspec/smdx"
+)
+
+// inspectCmd represents the inspect command
+var inspectCmd = &cobra.Command{
+	Use:   "inspect",
+	Short: "Inspect SunSpec device models and implemented values",
+	Long: `Inspect will iterate across the SunSpec model definition for the specified device and print details about defined models and data points.
+Limited to SunSpec TCP devices (EXPERIMENTAL).`,
+	Run: inspect,
+}
+
+func init() {
+	rootCmd.AddCommand(inspectCmd)
+
+	inspectCmd.PersistentFlags().StringSliceP(
+		"devices", "d",
+		[]string{},
+		`MODBUS device type and ID to query, multiple devices separated by comma or by repeating the flag.
+  Example: -d SDM:1,SDM:2 -d DZG:1.
+Valid types are:`+meterHelp()+`
+To use an adapter different from default, append RTU device or TCP address separated by @.
+If the adapter is a TCP connection (identified by :port), the device type (SUNS) is ignored and
+any type is considered valid.
+  Example: -d SDM:1@/dev/USB11 -d SMA:126@localhost:502`,
+	)
+}
+
+func pf(format string, v ...interface{}) {
+	format = strings.TrimSuffix(format, "\n") + "\n"
+	fmt.Printf(format, v...)
+}
+
+func scanSunspec(client modbus.Client) {
+	in, err := bus.Open(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+
+	in.Do(func(d sunspec.Device) {
+		d.Do(func(m sunspec.Model) {
+			pf("--------- Model %d %s ---------", m.Id(), modelName(m))
+
+			blocknum := 0
+			m.Do(func(b sunspec.Block) {
+				if blocknum > 0 {
+					fmt.Fprintln(tw, fmt.Sprintf("-- Block %d --", blocknum))
+				}
+				blocknum++
+
+				err = b.Read()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				b.Do(func(p sunspec.Point) {
+					t := p.Type()[0:3]
+					v := p.Value()
+					if p.NotImplemented() {
+						v = "n/a"
+					} else if t == "int" || t == "uin" || t == "acc" {
+						v = fmt.Sprintf("%.2f", p.ScaledValue())
+					}
+
+					vs := fmt.Sprintf("%17v", v)
+					fmt.Fprintln(tw, fmt.Sprintf("%s\t%s\t   %s", p.Id(), vs, p.Type()))
+				})
+			})
+
+			tw.Flush()
+		})
+	})
+}
+
+func modelName(m sunspec.Model) string {
+	model := smdx.GetModel(uint16(m.Id()))
+	if model == nil {
+		return ""
+	}
+	return model.Name
+}
+
+func printModel(m *smdx.ModelElement) {
+	pf("-- Definition --")
+	// pf("----")
+	// pf("Model:  %d - %s", m.Id, m.Name)
+	pf("Length: %d (0x%02x words, 0x%02x bytes)", m.Length, m.Length, 2*m.Length)
+	pf("Blocks: %d", len(m.Blocks))
+
+	for i, b := range m.Blocks {
+		pf("-- block #%d - %s", i, b.Name)
+		pf("Length: %d", b.Length)
+
+		for _, p := range b.Points {
+			u := ""
+			if p.Units != "" {
+				u = p.Units
+			}
+			pf("%4d %4d %12s %-8s %s", p.Offset, p.Length, p.Id, u, p.Type)
+		}
+	}
+}
+
+func inspect(cmd *cobra.Command, args []string) {
+	confHandler := NewDeviceConfigHandler()
+
+	// create default adapter from configuration
+	defaultDevice := viper.GetString("adapter")
+	if defaultDevice != "" {
+		confHandler.DefaultDevice = defaultDevice
+		confHandler.CreateAdapter(defaultDevice, viper.GetInt("baudrate"), viper.GetString("comset"))
+	}
+
+	// create devices from command line
+	devices, _ := cmd.PersistentFlags().GetStringSlice("devices")
+	if len(devices) == 0 {
+		fmt.Fprint(os.Stderr, "Missing device definition")
+	}
+	for _, dev := range devices {
+		if dev != "" {
+			confHandler.CreateDeviceFromSpec(dev)
+		}
+	}
+
+	for _, m := range confHandler.Managers {
+		m.All(func(id uint8, dev meters.Device) {
+			m.Conn.Slave(id)
+			scanSunspec(m.Conn.ModbusClient())
+		})
+	}
+}
