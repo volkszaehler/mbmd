1fbddb6ddf8829f53b24f0312f1281b8c085ed9f
andig <cpuidle@gmx.de>
2019-09-02 09:21:17 +0200

    Add additional registers (#31)
    - Total Power (W)
    - Total Apparent Power (W)
    - Total Reactive Power (W)
    - Total Import Power (W)
    - Net Energy (kWh)
    - Net Energy L1 (kWh)
    - Net Energy L2 (kWh)
    - Net Energy L3 (kWh)
    - Total Cosphi

--- meters/rs485/sdm.go
+++ meters/rs485/sdm.go
@@ -22,35 +22,45 @@ func NewSDMProducer() Producer {
 	 * some opcodes might not work on some devicep.
 	 */
 	ops := Opcodes{
-		VoltageL1: 0x0000,
-		VoltageL2: 0x0002,
-		VoltageL3: 0x0004,
-		CurrentL1: 0x0006,
-		CurrentL2: 0x0008,
-		CurrentL3: 0x000A,
-		PowerL1:   0x000C,
-		PowerL2:   0x000E,
-		PowerL3:   0x0010,
-		ImportL1:  0x015a,
-		ImportL2:  0x015c,
-		ImportL3:  0x015e,
-		Import:    0x0048,
-		ExportL1:  0x0160,
-		ExportL2:  0x0162,
-		ExportL3:  0x0164,
-		Export:    0x004a,
-		CosphiL1:  0x001e,
-		CosphiL2:  0x0020,
-		CosphiL3:  0x0022,
+		VoltageL1:     0x0000,
+		VoltageL2:     0x0002,
+		VoltageL3:     0x0004,
+		CurrentL1:     0x0006,
+		CurrentL2:     0x0008,
+		CurrentL3:     0x000A,
+		PowerL1:       0x000C,
+		PowerL2:       0x000E,
+		PowerL3:       0x0010,
+		Power:         0x0034,
+		ApparentPower: 0x0038,
+		ReactivePower: 0x003C,
+		ImportPower:   0x0054,
+		ImportL1:      0x015a,
+		ImportL2:      0x015c,
+		ImportL3:      0x015e,
+		Import:        0x0048,
+		ExportL1:      0x0160,
+		ExportL2:      0x0162,
+		ExportL3:      0x0164,
+		Export:        0x004a,
+		NetL1:         0x0066,
+		NetL2:         0x0068,
+		NetL3:         0x016a,
+		Net:           0x0156,
+		CosphiL1:      0x001e,
+		CosphiL2:      0x0020,
+		CosphiL3:      0x0022,
+		Cosphi:        0x003e,
+		THDL1:         0x00ea, // voltage
+		THDL2:         0x00ec, // voltage
+		THDL3:         0x00ee, // voltage
+		THD:           0x00F8, // voltage
+		Frequency:     0x0046,
 		//L1THDCurrent: 0x00F0, // current
 		//L2THDCurrent: 0x00F2, // current
 		//L3THDCurrent: 0x00F4, // current
 		//AvgTHDCurrent: 0x00Fa, // current
-		THDL1:     0x00ea, // voltage
-		THDL2:     0x00ec, // voltage
-		THDL3:     0x00ee, // voltage
-		THD:       0x00F8, // voltage
-		Frequency: 0x0046,
+		//ApparentImportPower: 0x0064,
 	}
 	return &SDMProducer{Opcodes: ops}
 }
