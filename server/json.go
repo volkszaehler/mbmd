654184178e4a1d982156eed97ae0701788f6ea5a
bilbolodz <bilbo@lodz.pl>
2020-01-04 09:19:12 +0100

    API return full precision floats

--- server/json.go
+++ server/json.go
@@ -58,8 +58,8 @@ func (s kvslice) MarshalJSON() ([]byte, error) {
 		// marshal value
 		var val []byte
 		switch kv.val.(type) {
-		case float64:
-			val = []byte(fmt.Sprintf("%.5g", kv.val))
+		case float32,float64:
+			val = []byte(fmt.Sprintf("%f", kv.val))
 		default:
 			if val, err = json.Marshal(kv.val); err != nil {
 				return nil, err
