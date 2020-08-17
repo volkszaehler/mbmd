642cf03200cd039c8f8c0b119392e6edabbdc342
andig <cpuidle@gmx.de>
2020-08-17 15:41:45 +0200

    Correct IEM3000 address offsets (#158)

--- meters/rs485/iem3000.go
+++ meters/rs485/iem3000.go
@@ -19,35 +19,35 @@ func NewIEM3000Producer() Producer {
 	 * https://download.schneider-electric.com/files?p_enDocType=User+guide&p_File_Name=DOCA0005DE-12.pdf&p_Doc_Ref=DOCA0005DE#page49
 	 */
 	ops := Opcodes{
-		VoltageL1: 3028,
-		VoltageL2: 3030,
-		VoltageL3: 3032,
-		Voltage:   3036,
+		VoltageL1: 0x0BD3,
+		VoltageL2: 0x0BD5,
+		VoltageL3: 0x0BD7,
+		Voltage:   0x0BDB,
 
-		CurrentL1: 3000,
-		CurrentL2: 3002,
-		CurrentL3: 3004,
-		Current:   3010,
+		CurrentL1: 0x0BB7,
+		CurrentL2: 0x0BB9,
+		CurrentL3: 0x0BBB,
+		Current:   0x0BC1,
 
-		PowerL1: 3054,
-		PowerL2: 3056,
-		PowerL3: 3058,
-		Power:   3060,
+		PowerL1: 0x0BED,
+		PowerL2: 0x0BEF,
+		PowerL3: 0x0BF1,
+		Power:   0x0BF3,
 
-		ReactivePower: 3068,
-		ApparentPower: 3076,
+		ReactivePower: 0x0BFB,
+		ApparentPower: 0x0C03,
 
-		// PowerFactor: 3084,
-		Frequency: 3110,
+		// PowerFactor: 0x0C0B,
+		Frequency: 0x0C25,
 
-		Import:   3204,
-		ImportL1: 3518,
-		ImportL2: 3522,
-		ImportL3: 3526,
-		Export:   3208,
+		Import:   0x0C83,
+		ImportL1: 0x0DBD,
+		ImportL2: 0x0DC1,
+		ImportL3: 0x0DC5,
+		Export:   0x0C87,
 
-		ReactiveImport: 3220,
-		ReactiveExport: 3224,
+		ReactiveImport: 0x0C93,
+		ReactiveExport: 0x0C97,
 	}
 	return &IEM3000Producer{Opcodes: ops}
 }
@@ -62,7 +62,7 @@ func (p *IEM3000Producer) Description() string {
 	return "Schneider Electric iEM3000 series"
 }
 
-func (p *IEM3000Producer) snipF(iec Measurement, scaler ...float64) Operation {
+func (p *IEM3000Producer) snipFloat32(iec Measurement, scaler ...float64) Operation {
 	snip := Operation{
 		FuncCode:  ReadHoldingReg,
 		OpCode:    p.Opcodes[iec],
@@ -78,9 +78,25 @@ func (p *IEM3000Producer) snipF(iec Measurement, scaler ...float64) Operation {
 	return snip
 }
 
+func (p *IEM3000Producer) snipInt64(iec Measurement, scaler ...float64) Operation {
+	snip := Operation{
+		FuncCode:  ReadHoldingReg,
+		OpCode:    p.Opcodes[iec],
+		ReadLen:   4,
+		IEC61850:  iec,
+		Transform: RTUInt64ToFloat64,
+	}
+
+	if len(scaler) > 0 {
+		snip.Transform = MakeScaledTransform(snip.Transform, scaler[0])
+	}
+
+	return snip
+}
+
 // Probe implements Producer interface
 func (p *IEM3000Producer) Probe() Operation {
-	return p.snipF(VoltageL1)
+	return p.snipFloat32(VoltageL1)
 }
 
 // Produce implements Producer interface
@@ -88,18 +104,11 @@ func (p *IEM3000Producer) Produce() (res []Operation) {
 	for op := range p.Opcodes {
 		switch op {
 		case PowerL1, PowerL2, PowerL3, Power, ReactivePower, ApparentPower:
-			res = append(res, p.snipF(op, 0.001))
+			res = append(res, p.snipFloat32(op, 0.001))
 		case Import, ImportL1, ImportL2, ImportL3, Export, ReactiveImport, ReactiveExport:
-			snip := Operation{
-				FuncCode:  ReadHoldingReg,
-				OpCode:    p.Opcodes[op],
-				ReadLen:   4,
-				IEC61850:  op,
-				Transform: MakeScaledTransform(RTUInt64ToFloat64, 1000),
-			}
-			res = append(res, snip)
+			res = append(res, p.snipInt64(op, 1000))
 		default:
-			res = append(res, p.snipF(op))
+			res = append(res, p.snipFloat32(op))
 		}
 	}
 
