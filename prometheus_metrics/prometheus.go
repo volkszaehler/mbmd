fa0fd2c91f940a9443c51f865b1769bbbe9ff2f7
kereis <kreis-dev@gmx.net>
2021-02-20 17:50:49 +0100

    feat(prometheus): Implement approach of dynamically adding counters/gauges of measurements
    Instead of creating a new counter/gauges for a measurement by hand, we simply just add a newly added measurement to the appropriate counterVeh/gaugesVeh map. This also means that everytime a new measurement in meters.Measurement is introduced, the maps in prometheus.go need to be updated as well.

--- prometheus_metrics/prometheus.go
+++ prometheus_metrics/prometheus.go
@@ -7,6 +7,7 @@ import (
 )
 
 const NAMESPACE = "mbmd"
+const SSN_MISSING = "NOT_AVAILABLE"
 
 var (
 	ConnectionAttemptTotal = prometheus.NewCounterVec(
@@ -159,132 +160,151 @@ var (
 		),
 		[]string{"device_id", "serial_number"},
 	)
+
+	MeasurementVoltage = prometheus.NewGaugeVec(
+		newGaugeOpts(
+			"measurement_voltage_volt",
+			"Last measurement of voltage in V",
+		),
+		[]string{"device_id", "serial_number"},
+	)
 )
 
+// counterVecMap contains all meters.Measurement that are associated with a prometheus.Counter
+//
+// If a new meters.Measurement is introduced, it needs to be added either to counterVecMap
+// or to gaugeVecMap - Otherwise Prometheus won't keep track of the newly added meters.Measurement
 var counterVecMap = map[meters.Measurement]*prometheus.CounterVec{
-	//meters.Sum:              {"Total Sum", "kWh"},
-	//meters.SumT1:            {"Tariff 1 Sum", "kWh"},
-	//meters.SumT2:            {"Tariff 2 Sum", "kWh"},
-	//meters.SumL1:            {"L1 Sum", "kWh"},
-	//meters.SumL2:            {"L2 Sum", "kWh"},
-	//meters.SumL3:            {"L3 Sum", "kWh"},
-	//meters.Import:           {"Total Import", "kWh"},
-	//meters.ImportT1:         {"Tariff 1 Import", "kWh"},
-	//meters.ImportT2:         {"Tariff 2 Import", "kWh"},
-	//meters.ImportL1:         {"L1 Import", "kWh"},
-	//meters.ImportL2:         {"L2 Import", "kWh"},
-	//meters.ImportL3:         {"L3 Import", "kWh"},
-	//meters.Export:           {"Total Export", "kWh"},
-	//meters.ExportT1:         {"Tariff 1 Export", "kWh"},
-	//meters.ExportT2:         {"Tariff 2 Export", "kWh"},
-	//meters.ExportL1:         {"L1 Export", "kWh"},
-	//meters.ExportL2:         {"L2 Export", "kWh"},
-	//meters.ExportL3:         {"L3 Export", "kWh"},
-	//meters.ReactiveSum:      {"Total Reactive", "kvarh"},
-	//meters.ReactiveSumT1:    {"Tariff 1 Reactive", "kvarh"},
-	//meters.ReactiveSumT2:    {"Tariff 2 Reactive", "kvarh"},
-	//meters.ReactiveSumL1:    {"L1 Reactive", "kvarh"},
-	//meters.ReactiveSumL2:    {"L2 Reactive", "kvarh"},
-	//meters.ReactiveSumL3:    {"L3 Reactive", "kvarh"},
-	//meters.ReactiveImport:   {"Reactive Import", "kvarh"},
-	//meters.ReactiveImportT1: {"Tariff 1 Reactive Import", "kvarh"},
-	//meters.ReactiveImportT2: {"Tariff 2 Reactive Import", "kvarh"},
-	//meters.ReactiveImportL1: {"L1 Reactive Import", "kvarh"},
-	//meters.ReactiveImportL2: {"L2 Reactive Import", "kvarh"},
-	//meters.ReactiveImportL3: {"L3 Reactive Import", "kvarh"},
-	//meters.ReactiveExport:   {"Reactive Export", "kvarh"},
-	//meters.ReactiveExportT1: {"Tariff 1 Reactive Export", "kvarh"},
-	//meters.ReactiveExportT2: {"Tariff 2 Reactive Export", "kvarh"},
-	//meters.ReactiveExportL1: {"L1 Reactive Export", "kvarh"},
-	//meters.ReactiveExportL2: {"L2 Reactive Export", "kvarh"},
-	//meters.ReactiveExportL3: {"L3 Reactive Export", "kvarh"},
-	//meters.DCEnergyS1:       {"String 1 Generation", "kWh"},
-	//meters.DCEnergyS2:       {"String 2 Generation", "kWh"},
-	//meters.DCEnergyS3:       {"String 3 Generation", "kWh"},
+	meters.Sum:              nil,
+	meters.SumT1:            nil,
+	meters.SumT2:            nil,
+	meters.SumL1:            nil,
+	meters.SumL2:            nil,
+	meters.SumL3:            nil,
+	meters.Import:           nil,
+	meters.ImportT1:         nil,
+	meters.ImportT2:         nil,
+	meters.ImportL1:         nil,
+	meters.ImportL2:         nil,
+	meters.ImportL3:         nil,
+	meters.Export:           nil,
+	meters.ExportT1:         nil,
+	meters.ExportT2:         nil,
+	meters.ExportL1:         nil,
+	meters.ExportL2:         nil,
+	meters.ExportL3:         nil,
+	meters.ReactiveSum:      nil,
+	meters.ReactiveSumT1:    nil,
+	meters.ReactiveSumT2:    nil,
+	meters.ReactiveSumL1:    nil,
+	meters.ReactiveSumL2:    nil,
+	meters.ReactiveSumL3:    nil,
+	meters.ReactiveImport:   nil,
+	meters.ReactiveImportT1: nil,
+	meters.ReactiveImportT2: nil,
+	meters.ReactiveImportL1: nil,
+	meters.ReactiveImportL2: nil,
+	meters.ReactiveImportL3: nil,
+	meters.ReactiveExport:   nil,
+	meters.ReactiveExportT1: nil,
+	meters.ReactiveExportT2: nil,
+	meters.ReactiveExportL1: nil,
+	meters.ReactiveExportL2: nil,
+	meters.ReactiveExportL3: nil,
+	meters.DCEnergyS1:       nil,
+	meters.DCEnergyS2:       nil,
+	meters.DCEnergyS3:       nil,
 }
 
+// gaugeVecMap contains all meters.Measurement that are associated with a prometheus.Gauge
+//
+// If a new meters.Measurement is introduced, it needs to be added either to counterVecMap
+// or to gaugeVecMap - Otherwise Prometheus won't keep track of the newly added meters.Measurement
 var gaugeVecMap = map[meters.Measurement]*prometheus.GaugeVec{
 	meters.Frequency:        MeasurementFrequency,
 	meters.Current:          MeasurementElectricCurrent,
 	meters.CurrentL1:        MeasurementL1Current,
 	meters.CurrentL2:        MeasurementL2Current,
 	meters.CurrentL3:        MeasurementL3Current,
-	//meters.Voltage:          {"Voltage", "V"},
-	//meters.VoltageL1:        {"L1 Voltage", "V"},
-	//meters.VoltageL2:        {"L2 Voltage", "V"},
-	//meters.VoltageL3:        {"L3 Voltage", "V"},
-	//meters.Power:            {"Power", "W"},
-	//meters.PowerL1:          {"L1 Power", "W"},
-	//meters.PowerL2:          {"L2 Power", "W"},
-	//meters.PowerL3:          {"L3 Power", "W"},
-	//meters.ImportPower:      {"Import Power", "W"},
-	//meters.ImportPowerL1:    {"L1 Import Power", "W"},
-	//meters.ImportPowerL2:    {"L2 Import Power", "W"},
-	//meters.ImportPowerL3:    {"L3 Import Power", "W"},
-	//meters.ExportPower:      {"Export Power", "W"},
-	//meters.ExportPowerL1:    {"L1 Export Power", "W"},
-	//meters.ExportPowerL2:    {"L2 Export Power", "W"},
-	//meters.ExportPowerL3:    {"L3 Export Power", "W"},
-	//meters.ReactivePower:    {"Reactive Power", "var"},
-	//meters.ReactivePowerL1:  {"L1 Reactive Power", "var"},
-	//meters.ReactivePowerL2:  {"L2 Reactive Power", "var"},
-	//meters.ReactivePowerL3:  {"L3 Reactive Power", "var"},
-	//meters.ApparentPower:    {"Apparent Power", "VA"},
-	//meters.ApparentPowerL1:  {"L1 Apparent Power", "VA"},
-	//meters.ApparentPowerL2:  {"L2 Apparent Power", "VA"},
-	//meters.ApparentPowerL3:  {"L3 Apparent Power", "VA"},
-	//meters.Cosphi:           {"Cosphi"},
-	//meters.CosphiL1:         {"L1 Cosphi"},
-	//meters.CosphiL2:         {"L2 Cosphi"},
-	//meters.CosphiL3:         {"L3 Cosphi"},
-	//meters.THD:              {"Average voltage to neutral THD", "%"},
-	//meters.THDL1:            {"L1 Voltage to neutral THD", "%"},
-	//meters.THDL2:            {"L2 Voltage to neutral THD", "%"},
-	//meters.THDL3:            {"L3 Voltage to neutral THD", "%"},
-	//meters.DCCurrent:        {"DC Current", "A"},
-	//meters.DCVoltage:        {"DC Voltage", "V"},
-	//meters.DCPower:          {"DC Power", "W"},
-	//meters.HeatSinkTemp:     {"Heat Sink Temperature", "°C"},
-	//meters.DCCurrentS1:      {"String 1 Current", "A"},
-	//meters.DCVoltageS1:      {"String 1 Voltage", "V"},
-	//meters.DCPowerS1:        {"String 1 Power", "W"},
-	//meters.DCCurrentS2:      {"String 2 Current", "A"},
-	//meters.DCVoltageS2:      {"String 2 Voltage", "V"},
-	//meters.DCPowerS2:        {"String 2 Power", "W"},
-	//meters.DCCurrentS3:      {"String 3 Current", "A"},
-	//meters.DCVoltageS3:      {"String 3 Voltage", "V"},
-	//meters.DCPowerS3:        {"String 3 Power", "W"},
-	//meters.ChargeState:      {"Charge State", "%"},
-	//meters.BatteryVoltage:   {"Battery Voltage", "V"},
-	//meters.PhaseAngle:       {"Phase Angle", "°"},
+	meters.Voltage:          MeasurementVoltage,
+	meters.VoltageL1: 		 nil,
+	meters.VoltageL2:        nil,
+	meters.VoltageL3:        nil,
+	meters.Power:            nil,
+	meters.PowerL1:          nil,
+	meters.PowerL2:          nil,
+	meters.PowerL3:          nil,
+	meters.ImportPower:      nil,
+	meters.ImportPowerL1:    nil,
+	meters.ImportPowerL2:    nil,
+	meters.ImportPowerL3:    nil,
+	meters.ExportPower:      nil,
+	meters.ExportPowerL1:    nil,
+	meters.ExportPowerL2:    nil,
+	meters.ExportPowerL3:    nil,
+	meters.ReactivePower:    nil,
+	meters.ReactivePowerL1:  nil,
+	meters.ReactivePowerL2:  nil,
+	meters.ReactivePowerL3:  nil,
+	meters.ApparentPower:    nil,
+	meters.ApparentPowerL1:  nil,
+	meters.ApparentPowerL2:  nil,
+	meters.ApparentPowerL3:  nil,
+	meters.Cosphi:           nil,
+	meters.CosphiL1:         nil,
+	meters.CosphiL2:         nil,
+	meters.CosphiL3:         nil,
+	meters.THD:              nil,
+	meters.THDL1:            nil,
+	meters.THDL2:            nil,
+	meters.THDL3:            nil,
+	meters.DCCurrent:        nil,
+	meters.DCVoltage:        nil,
+	meters.DCPower:          nil,
+	meters.HeatSinkTemp:     nil,
+	meters.DCCurrentS1:      nil,
+	meters.DCVoltageS1:      nil,
+	meters.DCPowerS1:        nil,
+	meters.DCCurrentS2:      nil,
+	meters.DCVoltageS2:      nil,
+	meters.DCPowerS2:        nil,
+	meters.DCCurrentS3:      nil,
+	meters.DCVoltageS3:      nil,
+	meters.DCPowerS3:        nil,
+	meters.ChargeState:      nil,
+	meters.BatteryVoltage:   nil,
+	meters.PhaseAngle:       nil,
 }
 
 // Init registers all globally defined metrics to Prometheus library's default registry
 func Init() {
-	prometheus.MustRegister(
-		//ConnectionAttemptTotal,
-		//ConnectionAttemptFailedTotal,
-		//ConnectionPartiallySuccessfulTotal,
-		//ReadDeviceDetailsFailedTotal,
-		//DevicesCreatedTotal,
+	//prometheus.MustRegister(
+	//	//ConnectionAttemptTotal,
+	//	//ConnectionAttemptFailedTotal,
+	//	//ConnectionPartiallySuccessfulTotal,
+	//	//ReadDeviceDetailsFailedTotal,
+	//	//DevicesCreatedTotal,
+	//
+	//	// Device specific actions
+	//	DeviceQueriesTotal,
+	//	DeviceQueriesErrorTotal,
+	//	DeviceQueriesSuccessTotal,
+	//	DeviceQueryMeasurementValueSkippedTotal,
+	//
+	//	// Bus scan metrics of cmd.scan
+	//	BusScanTotal,
+	//	BusScanDeviceProbeSuccessfulTotal,
+	//	BusScanDeviceProbeFailedTotal,
+	//
+	//	// Measurement gauges
+	//	MeasurementElectricCurrent,
+	//	MeasurementL1Current,
+	//	MeasurementL2Current,
+	//	MeasurementL3Current,
+	//)
 
-		// Device specific actions
-		DeviceQueriesTotal,
-		DeviceQueriesErrorTotal,
-		DeviceQueriesSuccessTotal,
-		DeviceQueryMeasurementValueSkippedTotal,
-
-		// Bus scan metrics of cmd.scan
-		BusScanTotal,
-		BusScanDeviceProbeSuccessfulTotal,
-		BusScanDeviceProbeFailedTotal,
-
-		// Measurement gauges
-		MeasurementElectricCurrent,
-		MeasurementL1Current,
-		MeasurementL2Current,
-		MeasurementL3Current,
-	)
+	initAndRegisterGauges()
+	initAndRegisterCounters()
 }
 
 // UpdateMeasurementMetric updates a counter or gauge based by passed measurement
@@ -295,7 +315,7 @@ func UpdateMeasurementMetric(
 ) {
 	// TODO Remove when development is finished or think about a solution handling mocked devices
 	if deviceSerial == "" {
-		deviceSerial = "mock??"
+		deviceSerial = SSN_MISSING
 	}
 
 	fmt.Printf("prometheus> [%s] deviceSerial: %s, measurement: %s\n", deviceId, deviceSerial, measurement.Value)
@@ -324,4 +344,50 @@ func newGaugeOpts(name string, help string) prometheus.GaugeOpts {
 		Name:      name,
 		Help:      help,
 	}
+}
+
+func initAndRegisterCounters() {
+	//collectors := make([]prometheus.Collector, len(counterVecMap))
+
+	for measurement := range counterVecMap {
+		newCounter := prometheus.NewCounterVec(
+			newCounterOpts(
+				measurement.PrometheusName(),
+				prometheusDescription(measurement),
+			),
+			[]string{"device_id", "serial_number"},
+		)
+		counterVecMap[measurement] = newCounter
+		//collectors = append(collectors, newCounter)
+		prometheus.MustRegister(newCounter)
+	}
+
+	// fmt.Print("Registering new collectors of type CounterVec")
+	//prometheus.MustRegister(collectors...)
+}
+
+func initAndRegisterGauges() {
+	// gauges := make([]*prometheus.GaugeVec, len(gaugeVecMap))
+
+	for measurement := range gaugeVecMap {
+		newGauge := prometheus.NewGaugeVec(
+			newGaugeOpts(
+				measurement.PrometheusName(),
+				prometheusDescription(measurement),
+			),
+			[]string{"device_id", "serial_number"},
+		)
+		gaugeVecMap[measurement] = newGauge
+		// gauges = append(gauges, newGauge)
+		prometheus.MustRegister(newGauge)
+	}
+
+	//fmt.Print("Registering new collectors of type GaugeVec")
+
+}
+
+func prometheusDescription(measurement meters.Measurement) string {
+	description, unit := measurement.DescriptionAndUnit()
+
+	return fmt.Sprintf("Measurement of %s in %s", description, unit)
 }
