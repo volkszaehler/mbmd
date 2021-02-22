34ef53d3ac2165aa76bee0fbf1f9ccf2fbc1049b
kereis <kreis-dev@gmx.net>
2021-02-22 09:14:34 +0100

    feat(prometheus): Refactor Unit in measurements.go to dedicated enum

---
+++ meters/units.go
@@ -0,0 +1,87 @@
+package meters
+
+// Unit is used to represent measurements
+type Unit int
+
+//go:generate enumer -type=Unit -transform=snake
+const (
+	_ Unit = iota
+
+	KiloVarHour
+	Var
+	KiloWattHour
+	Watt
+
+	Ampere
+	Volt
+	VoltAmpere
+
+	Degree
+	DegreeCelsius
+
+	Hertz
+
+	Percent
+
+	NoUnit
+)
+
+type unit struct {
+	Abbreviation 	unitAbbreviation
+	PluralForm		string
+}
+
+// unitAbbreviation defines the default abbreviation and - if needed - an alternative.
+type unitAbbreviation struct {
+	Default     string
+	Alternative string
+}
+
+var units = map[Unit]unit{
+	KiloVarHour: 	{unitAbbreviation{"kvarh", ""}, "kilo"},
+	Var:			{unitAbbreviation{"var", ""}, ""},
+	KiloWattHour:   {unitAbbreviation{"kWh", ""}, ""},
+	Watt: 			{unitAbbreviation{"W", ""}, ""},
+	Ampere: 		{unitAbbreviation{"A", ""}, ""},
+	Volt: 			{unitAbbreviation{"V", ""}, ""},
+	VoltAmpere: 	{unitAbbreviation{"VA", ""}, ""},
+	Degree: 		{unitAbbreviation{"°", "degree"}, ""},
+	DegreeCelsius: 	{unitAbbreviation{"°C", "degree_celsius"}, ""},
+	Hertz:			{unitAbbreviation{"Hz", "hertz"}, "hertz"},
+	Percent:		{unitAbbreviation{"%", "percent"}, "percent"},
+
+	NoUnit:			{unitAbbreviation{"", ""}, ""},
+}
+
+func (u Unit) PrometheusName() string {
+	switch u {
+	case Hertz:
+		return "hertz"
+	case DegreeCelsius:
+		return "degrees_celsius"
+	case NoUnit:
+		return ""
+	default:
+		return u.String() + "s"
+	}
+}
+
+// Abbreviation returns the matching abbreviation of a Unit if it exists
+func (u Unit) Abbreviation() string {
+	if unit, ok := units[u]; ok {
+		return unit.Abbreviation.Default
+	}
+	return ""
+}
+
+// AlternativeAbbreviation returns the matching alternative abbreviation of a Unit if it exists
+func (u Unit) AlternativeAbbreviation() string {
+	if unit, ok := units[u]; ok {
+		alternative := unit.Abbreviation.Alternative
+		if alternative != "" {
+			return alternative
+		}
+	}
+	return ""
+}
+
