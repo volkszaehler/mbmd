1287bc3dc55b04bf3bf6d9e0215231f9985aeb06
kereis <kreis-dev@gmx.net>
2021-02-26 11:03:57 +0100

    feat(prometheus): Add socket metrics and categorize static metrics
    Each static metrics is now categorized by interface `collectable`. `collectable.Collect()` will be called when all static metrics are registered to the default Prometheus registry.

---
+++ prometheus_metrics/collectable.go
@@ -0,0 +1,8 @@
+package prometheus_metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+// collectable is a generic interface that collects all "categorized" Prometheus metrics into a slice.
+type collectable interface {
+	Collect() []prometheus.Collector
+}
