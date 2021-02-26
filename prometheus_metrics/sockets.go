4963725258e0e602ecd1990d819a9ecabe6db197
kereis <kreis-dev@gmx.net>
2021-02-26 11:03:57 +0100

    feat(prometheus): Add socket metrics and categorize static metrics
    Each static metrics is now categorized by interface `collectable`. `collectable.Collect()` will be called when all static metrics are registered to the default Prometheus registry.

---
+++ prometheus_metrics/sockets.go
@@ -0,0 +1,66 @@
+package prometheus_metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+// socketCollectors contains all Prometheus metrics about web sockets
+type socketCollectors struct {
+	collectable
+}
+
+var (
+	WebSocketClientConnectionClose = prometheus.NewCounter(
+		*newCounterOpts(
+			"websocket_client_connection_closed_total",
+			"Total amount of closed client connection to a web socket",
+		),
+	)
+
+	WebSocketClientMessageSendSuccess = prometheus.NewCounter(
+		*newCounterOpts(
+			"websocket_client_message_send_success_total",
+			"Total amount of messages sent to a web socket client",
+		),
+	)
+
+	WebSocketClientMessageSendFailure = prometheus.NewCounter(
+		*newCounterOpts(
+			"websocket_client_message_send_failure_total",
+			"",
+		),
+	)
+
+	WebSocketClientCreationFailure = prometheus.NewCounterVec(
+		*newCounterOpts(
+			"websocket_client_creation_failure_total",
+			"",
+		),
+		[]string{"creation_type"},
+	)
+
+	WebSocketClientCreationSuccess = prometheus.NewCounterVec(
+		*newCounterOpts(
+			"websocket_client_creation_success_total",
+			"",
+		),
+		[]string{"creation_type"},
+	)
+
+	WebSocketMessageBytesSent = prometheus.NewCounter(
+		*newCounterOpts(
+			"websocket_message_bytes_sent_total",
+			"",
+		),
+	)
+)
+
+func (c socketCollectors) Collect() []prometheus.Collector {
+	return []prometheus.Collector{
+		WebSocketClientConnectionClose,
+		WebSocketClientMessageSendSuccess,
+		WebSocketClientMessageSendFailure,
+		WebSocketClientCreationFailure,
+		WebSocketClientCreationSuccess,
+		WebSocketMessageBytesSent,
+	}
+}
+
