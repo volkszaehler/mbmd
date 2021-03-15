ba4f34dc21f1becb506ff4918857032b354f87aa
kereis <kreis-dev@gmx.net>
2021-03-15 14:59:27 +0100

    feat(prometheus): Integrate device name (by config) for unique metric ID
    If no device description is available, we cannot distinguish between all device structs which it makes also difficult to distinguish between metrics.
    
    Therefore I add the device name that the config specifies in order to make them "comparable"

--- prometheus_metrics/register.go
+++ prometheus_metrics/register.go
@@ -7,20 +7,12 @@ import (
 
 // Static metrics
 var (
-	BusScanStartedTotal = prometheus.NewCounterVec(
-		*newCounterOpts(
-			"bus_scan_started_total",
-			"Total started bus scans",
-		),
-		[]string{"device_id"},
-	)
-
 	BusScanDeviceInitializationErrorTotal = prometheus.NewCounterVec(
 		*newCounterOpts(
 			"bus_scan_device_initialization_error_total",
 			"Total errors upon initialization of a device during bus scan",
 		),
-		[]string{"device_id"},
+		[]string{"device_name"},
 	)
 
 	BusScanDeviceProbeSuccessfulTotal = prometheus.NewCounterVec(
@@ -28,7 +20,7 @@ var (
 			"bus_scan_device_probe_successful_total",
 			"Amount of successfully found devices during bus scan",
 		),
-		[]string{"serial_number"},
+		[]string{"device_name", "serial_number"},
 	)
 
 	BusScanDeviceProbeFailedTotal = prometheus.NewCounterVec(
@@ -36,15 +28,7 @@ var (
 			"bus_scan_device_probe_failed_total",
 			"Amount of devices failed to be found during bus scan",
 		),
-		[]string{"device_id"},
-	)
-
-	ReadDeviceDetailsFailedTotal = prometheus.NewCounterVec(
-		*newCounterOpts(
-			"smart_meter_read_device_details_failed_total",
-			"Reading additional details of a smart meter failed",
-		),
-		[]string{"model"},
+		[]string{"device_name"},
 	)
 )
 
