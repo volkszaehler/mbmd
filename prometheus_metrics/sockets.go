b2d5c5110b0f3b181c915cb0adc78f118c28efa6
kereis <kreis-dev@gmx.net>
2021-03-19 17:50:40 +0100

    feat(prometheus): Change some metrics descriptions
    This is purely a change of expressions

--- prometheus_metrics/sockets.go
+++ prometheus_metrics/sockets.go
@@ -5,42 +5,42 @@ import "github.com/prometheus/client_golang/prometheus"
 // socketCollectors contains all Prometheus metrics about web sockets
 //
 // Implements collectable interface
-type socketCollectors struct {}
+type socketCollectors struct{}
 
 var (
 	WebSocketClientConnectionClose = prometheus.NewCounter(
 		*newCounterOpts(
-			"websocket_client_connection_closed_total",
-			"Total amount of closed client connection to a web socket",
+			"websocket_client_connections_closed_total",
+			"Total number of closed client connections to a web socket",
 		),
 	)
 
 	WebSocketClientMessageSendSuccess = prometheus.NewCounter(
 		*newCounterOpts(
-			"websocket_client_message_send_success_total",
-			"Total amount of messages sent to a web socket client",
+			"websocket_client_message_send_successes_total",
+			"Total number of messages sent to a web socket client",
 		),
 	)
 
 	WebSocketClientMessageSendFailure = prometheus.NewCounter(
 		*newCounterOpts(
-			"websocket_client_message_send_failure_total",
-			"Total count of message send failures to a web socket client",
+			"websocket_client_message_send_failures_total",
+			"Total number of message send failures to a web socket client",
 		),
 	)
 
 	WebSocketClientCreationFailure = prometheus.NewCounterVec(
 		*newCounterOpts(
-			"websocket_client_creation_failure_total",
-			"Total count of accepting and failed creation of a web socket client",
+			"websocket_client_creation_failures_total",
+			"Total number of accepting and failed creation of a web socket client",
 		),
 		[]string{"creation_type"},
 	)
 
 	WebSocketClientCreationSuccess = prometheus.NewCounterVec(
 		*newCounterOpts(
-			"websocket_client_creation_success_total",
-			"Total count of accepting and successful creation of a web socket client",
+			"websocket_client_creation_successes_total",
+			"Total number of accepting and successful creation of a web socket client",
 		),
 		[]string{"creation_type"},
 	)
@@ -48,7 +48,7 @@ var (
 	WebSocketMessageBytesSent = prometheus.NewCounter(
 		*newCounterOpts(
 			"websocket_message_bytes_sent_total",
-			"Total bytes sent to web socket clients",
+			"Total number of bytes sent to web socket clients",
 		),
 	)
 )
