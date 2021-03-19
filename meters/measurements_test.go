a35794e2305f437e65baf5bd70e85fbe7709ab33
kereis <kreis-dev@gmx.net>
2021-03-19 12:05:11 +0100

    feat(prometheus): Add tests for Prometheus unit and conversion

--- meters/measurements_test.go
+++ meters/measurements_test.go
@@ -2,6 +2,7 @@ package meters
 
 import (
 	"testing"
+	"time"
 )
 
 func TestMeasurementCreation_WithRequiredOptions_WithMetricType_Counter(t *testing.T) {
@@ -62,5 +63,68 @@ func TestMeasurementCreation_WithCustomName_AndDescription(t *testing.T) {
 			measurement.PrometheusInfo.Description,
 			expectedDescription,
 		)
+	}
+}
+
+func TestMeasurementCreation_WithUnitInPrometheusSpecified(t *testing.T) {
+	measurement := newInternalMeasurement(
+		withDescription("My Test Measurement"),
+		withPrometheusDescription("My custom description for my measurement"),
+		withPrometheusName("my_custom_name_for_my_test_measurement"),
+		withUnit(KiloWattHour),
+		withUnitInPrometheus(Joule),
+		withMetricType(Counter),
+	)
+
+	expectedPrometheusName := "measurement_my_custom_name_for_my_test_measurement_joules_total"
+	expectedDescription := "My custom description for my measurement"
+
+	if measurement.PrometheusInfo.Name != expectedPrometheusName {
+		t.Errorf(
+			"Prometheus metric name '%s' does not equal expected '%s'",
+			measurement.PrometheusInfo.Name,
+			expectedPrometheusName,
+		)
+	}
+
+	if measurement.PrometheusInfo.Description != expectedDescription {
+		t.Errorf("Prometheus description '%s' does not equal expected '%s'",
+			measurement.PrometheusInfo.Description,
+			expectedDescription,
+		)
+	}
+}
+
+func TestMeasurementResult_ConvertValueTo(t *testing.T) {
+	measurementResult := &MeasurementResult{
+		Measurement: Export,
+		Value:       100.0,
+		Timestamp:   time.Time{},
+	}
+
+	expected := 360_000_000.0
+	actual := measurementResult.ConvertValueTo(Joule)
+
+	if expected != actual {
+		t.Errorf("Expected conversion value '%f' does not equal actual value '%f'",
+			expected,
+			actual)
+	}
+}
+
+func TestMeasurementResult_ConvertValueTo_NonExistingConversionFunc(t *testing.T) {
+	measurementResult := &MeasurementResult{
+		Measurement: Export,
+		Value:       100.0,
+		Timestamp:   time.Time{},
+	}
+
+	expected := 0.0
+	actual := measurementResult.ConvertValueTo(NoUnit)
+
+	if expected != actual {
+		t.Errorf("Expected conversion value '%f' does not equal actual value '%f'",
+			expected,
+			actual)
 	}
 }
