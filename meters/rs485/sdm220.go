f01f9979ec446d6ac05c465dcded0b25de178af9
Hans Cornelis <hacornelis@gmail.com>
2021-07-06 16:59:24 +0200

    Updated spaces vs tabs editor difference

--- meters/rs485/sdm220.go
+++ meters/rs485/sdm220.go
@@ -24,12 +24,12 @@ func NewSDM220Producer() Producer {
 		Voltage:        0x0000, // 220, 230
 		Current:        0x0006, // 220, 230
 		Power:			0x000c, // 220
-        ApparentPower:  0x0012, // 220
-        ReactivePower:  0x0018, // 220
-        Cosphi: 		0x0024, // 220
-        Frequency:      0x0046, // 220
-		Import:         0x0048, // 220, 230
-		Export:         0x004a, // 220, 230
+		ApparentPower:  0x0012, // 220
+		ReactivePower:	0x0018, // 220
+		Cosphi: 		0x0024, // 220
+		Frequency:		0x0046, // 220
+		Import:			0x0048, // 220, 230
+		Export:			0x004a, // 220, 230
 		Sum:            0x0156, // 220, 230
 		ReactiveSum:    0x0158, // 220
 		ReactiveImport: 0x4C,   // 220, 230
