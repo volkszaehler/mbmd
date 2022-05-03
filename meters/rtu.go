235454b9790f25a4f24f66a97dab1d8abb15743a
Richard van den Berg <richard@vdberg.org>
2022-05-03 16:22:53 +0200

    Increase timeout to 600 ms

--- meters/rtu.go
+++ meters/rtu.go
@@ -33,7 +33,7 @@ func NewClientHandler(device string, baudrate int, comset string) *modbus.RTUCli
 		log.Fatalf("Invalid communication set specified: %s. See -h for help.", comset)
 	}
 
-	handler.Timeout = 300 * time.Millisecond
+	handler.Timeout = 600 * time.Millisecond
 
 	return handler
 }
