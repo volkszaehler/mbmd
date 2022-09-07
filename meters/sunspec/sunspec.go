f715cedbb7892ed51f80f067465bdd8261cf7304
andig <cpuidle@gmx.de>
2022-09-07 17:59:33 +0200

    Sunspec: make QueryPoint honour measurement scaling (#279)

--- meters/sunspec/sunspec.go
+++ meters/sunspec/sunspec.go
@@ -199,7 +199,7 @@ func (d *SunSpec) notInitialized() bool {
 	return len(d.models) == 0
 }
 
-func (d *SunSpec) convertPoint(b sunspec.Block, p sunspec.Point, m meters.Measurement) (meters.MeasurementResult, error) {
+func (d *SunSpec) convertPoint(b sunspec.Block, p sunspec.Point) (float64, error) {
 	if d.descriptor.Manufacturer == "KOSTAL" {
 		FixKostal(p)
 	}
@@ -207,21 +207,10 @@ func (d *SunSpec) convertPoint(b sunspec.Block, p sunspec.Point, m meters.Measur
 	v := p.ScaledValue()
 
 	if math.IsNaN(v) {
-		return meters.MeasurementResult{}, fmt.Errorf("%w", meters.ErrNaN)
+		return 0, meters.ErrNaN
 	}
 
-	// apply scale factor for energy
-	if div, ok := dividerMap[m]; ok {
-		v /= div
-	}
-
-	mr := meters.MeasurementResult{
-		Measurement: m,
-		Value:       v,
-		Timestamp:   time.Now(),
-	}
-
-	return mr, nil
+	return v, nil
 }
 
 // QueryPoint executes a single query operation for model/block/point on the bus
@@ -262,17 +251,34 @@ func (d *SunSpec) QueryPointAny(client modbus.Client, modelID, blockID int, poin
 	return block, point, fmt.Errorf("sunspec: %d:%d:%s not found", modelID, blockID, pointID)
 }
 
-// QueryPoint executes a single query operation for model/block/point on the bus
-func (d *SunSpec) QueryPoint(client modbus.Client, modelID, blockID int, pointID string) (res meters.MeasurementResult, err error) {
+// QueryPoint executes a single query operation for model/block/point on the bus.
+// The result is returned as-is, i.e. not scaled.
+func (d *SunSpec) QueryPoint(client modbus.Client, modelID, blockID int, pointID string) (float64, error) {
 	block, point, err := d.QueryPointAny(client, modelID, blockID, pointID)
 	if err != nil {
-		return res, err
+		return 0, err
 	}
 
-	return d.convertPoint(block, point, meters.Measurement(0))
+	return d.convertPoint(block, point)
 }
 
-// QueryOp queries all models and blocks until measurement is found
+func makeResult(v float64, m meters.Measurement) meters.MeasurementResult {
+	// apply scale factor for energy
+	if div, ok := dividerMap[m]; ok {
+		v /= div
+	}
+
+	res := meters.MeasurementResult{
+		Measurement: m,
+		Value:       v,
+		Timestamp:   time.Now(),
+	}
+
+	return res
+}
+
+// QueryOp queries all models and blocks until measurement is found.
+// The result is scaled as defined in the divider map.
 func (d *SunSpec) QueryOp(client modbus.Client, measurement meters.Measurement) (res meters.MeasurementResult, err error) {
 	if d.notInitialized() {
 		return res, errors.New("sunspec: not initialized")
@@ -291,8 +297,13 @@ func (d *SunSpec) QueryOp(client modbus.Client, measurement meters.Measurement) 
 
 				for pointID, m := range pointMap {
 					if m == measurement {
-						mr, err := d.QueryPoint(client, int(modelID), blockID, pointID)
-						mr.Measurement = measurement
+						v, err := d.QueryPoint(client, int(modelID), blockID, pointID)
+
+						var mr meters.MeasurementResult
+						if err == nil {
+							mr = makeResult(v, measurement)
+						}
+
 						return mr, err
 					}
 				}
@@ -304,6 +315,7 @@ func (d *SunSpec) QueryOp(client modbus.Client, measurement meters.Measurement) 
 }
 
 // Query is called by the handler after preparing the bus by setting the device id and waiting for rate limit
+// The results are scaled as defined in the divider map.
 func (d *SunSpec) Query(client modbus.Client) (res []meters.MeasurementResult, err error) {
 	if d.notInitialized() {
 		return res, errors.New("sunspec: not initialized")
@@ -342,8 +354,8 @@ func (d *SunSpec) Query(client modbus.Client) (res []meters.MeasurementResult, e
 				for pointID, m := range pointMap {
 					point := block.MustPoint(pointID)
 
-					if mr, err := d.convertPoint(block, point, m); err == nil {
-						res = append(res, mr)
+					if v, err := d.convertPoint(block, point); err == nil {
+						res = append(res, makeResult(v, m))
 					}
 				}
 			}
