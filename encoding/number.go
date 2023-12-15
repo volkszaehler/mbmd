3bee36cf2c6cb275d1d3ebd5e77d82057e044b34
andig <cpuidle@gmx.de>
2023-12-15 09:37:39 +0100

    Add write encodings

--- encoding/number.go
+++ encoding/number.go
@@ -10,9 +10,17 @@ func Uint16(b []byte) uint16 {
 	return binary.BigEndian.Uint16(b)
 }
 
+func PutUint16(b []byte, v uint16) {
+	binary.BigEndian.PutUint16(b, v)
+}
+
 // Int16 decodes bytes as int16 in network byte order (big endian)
 func Int16(b []byte) int16 {
 	return int16(binary.BigEndian.Uint16(b))
+}
+
+func PutInt16(b []byte, v int16) {
+	binary.BigEndian.PutUint16(b, uint16(v))
 }
 
 // Uint32 decodes bytes as uint32 in network byte order (big endian)
@@ -20,9 +28,17 @@ func Uint32(b []byte) uint32 {
 	return binary.BigEndian.Uint32(b)
 }
 
+func PutUint32(b []byte, v uint32) {
+	binary.BigEndian.PutUint32(b, v)
+}
+
 // Int32 decodes bytes as int32 in network byte order (big endian)
 func Int32(b []byte) int32 {
 	return int32(binary.BigEndian.Uint32(b))
+}
+
+func PutInt32(b []byte, v int32) {
+	binary.BigEndian.PutUint32(b, uint32(v))
 }
 
 // Uint64 decodes bytes as uint64 in network byte order (big endian)
@@ -30,9 +46,17 @@ func Uint64(b []byte) uint64 {
 	return binary.BigEndian.Uint64(b)
 }
 
+func PutUint64(b []byte, v uint64) {
+	binary.BigEndian.PutUint64(b, v)
+}
+
 // Int64 decodes bytes as int64 in network byte order (big endian)
 func Int64(b []byte) int64 {
 	return int64(binary.BigEndian.Uint64(b))
+}
+
+func PutInt64(b []byte, v int64) {
+	binary.BigEndian.PutUint64(b, uint64(v))
 }
 
 // Float32 decodes bytes as float32 in network byte order (big endian)
@@ -40,9 +64,17 @@ func Float32(b []byte) float32 {
 	return math.Float32frombits(Uint32(b))
 }
 
+func PutFloat32(b []byte, v float32) {
+	binary.BigEndian.PutUint32(b, math.Float32bits(v))
+}
+
 // Float64 decodes bytes as float64 in network byte order (big endian)
 func Float64(b []byte) float64 {
 	return math.Float64frombits(Uint64(b))
+}
+
+func PutFloat64(b []byte, v float64) {
+	binary.BigEndian.PutUint64(b, math.Float64bits(v))
 }
 
 // Uint32LswFirst decodes bytes as uint32 in network byte order (big endian) with least significant word first
@@ -51,15 +83,27 @@ func Uint32LswFirst(b []byte) uint32 {
 	return uint32(b[2])<<24 | uint32(b[3])<<16 | uint32(b[0])<<8 | uint32(b[1])
 }
 
+func PutUint32LswFirst(b []byte, v uint32) {
+	binary.BigEndian.PutUint32(b, v<<16|v>>16)
+}
+
 // Int32LswFirst decodes bytes as int32 in network byte order (big endian) with least significant word first
 func Int32LswFirst(b []byte) int32 {
 	_ = b[3] // bounds check hint to compiler; see golang.org/issue/14808
 	return int32(uint32(b[2])<<24 | uint32(b[3])<<16 | uint32(b[0])<<8 | uint32(b[1]))
 }
 
+func PutInt32LswFirst(b []byte, v int32) {
+	PutUint32LswFirst(b, uint32(v))
+}
+
 // Float32LswFirst decodes bytes as float32 in network byte order (big endian) with least significant word first
 func Float32LswFirst(b []byte) float32 {
 	return math.Float32frombits(Uint32LswFirst(b))
+}
+
+func PutFloat32LswFirst(b []byte, v float32) {
+	PutUint32LswFirst(b, math.Float32bits(v))
 }
 
 // Uint64LswFirst decodes bytes as uint64 in network byte order (big endian) with least significant word first
@@ -68,13 +112,26 @@ func Uint64LswFirst(b []byte) uint64 {
 	return uint64(b[6])<<56 | uint64(b[7])<<48 | uint64(b[4])<<40 | uint64(b[5])<<32 | uint64(b[2])<<24 | uint64(b[3])<<16 | uint64(b[0])<<8 | uint64(b[1])
 }
 
+func PutUint64LswFirst(b []byte, v uint64) {
+	PutUint32LswFirst(b, uint32(v))
+	PutUint32LswFirst(b[4:], uint32(v>>32))
+}
+
 // Int64LswFirst decodes bytes as int64 in network byte order (big endian) with least significant word first
 func Int64LswFirst(b []byte) int64 {
 	_ = b[7] // bounds check hint to compiler; see golang.org/issue/14808
 	return int64(b[6])<<56 | int64(b[7])<<48 | int64(b[4])<<40 | int64(b[5])<<32 | int64(b[2])<<24 | int64(b[3])<<16 | int64(b[0])<<8 | int64(b[1])
 }
 
+func PutInt64LswFirst(b []byte, v int64) {
+	PutUint64LswFirst(b, uint64(v))
+}
+
 // Float64LswFirst decodes bytes as float64 in network byte order (big endian) with least significant word first
 func Float64LswFirst(b []byte) float64 {
 	return math.Float64frombits(Uint64LswFirst(b))
+}
+
+func PutFloat64LswFirst(b []byte, v float64) {
+	PutUint64LswFirst(b, math.Float64bits(v))
 }
--- encoding/number_test.go
+++ encoding/number_test.go
@@ -1,11 +1,120 @@
 package encoding
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	u16 = uint16(0x1234)
+	i16 = int16(0x1234)
+	u32 = uint32(0x12345678)
+	i32 = int32(0x12345678)
+	u64 = uint64(0x1234567812345678)
+	i64 = int64(0x1234567812345678)
+)
+
+func TestUint16(t *testing.T) {
+	b := make([]byte, 2)
+
+	PutUint16(b, u16)
+	assert.Equal(t, u16, Uint16(b))
+}
+
+func TestInt16(t *testing.T) {
+	b := make([]byte, 2)
+
+	PutInt16(b, i16)
+	assert.Equal(t, i16, Int16(b))
+}
+
+func TestUint32(t *testing.T) {
+	b := make([]byte, 4)
+
+	PutUint32(b, u32)
+	assert.Equal(t, u32, Uint32(b))
+}
+
+func TestInt32(t *testing.T) {
+	b := make([]byte, 4)
+
+	PutInt32(b, i32)
+	assert.Equal(t, i32, Int32(b))
+}
+
+func TestUint64(t *testing.T) {
+	b := make([]byte, 8)
+
+	PutUint64(b, u64)
+	assert.Equal(t, u64, Uint64(b))
+}
+
+func TestInt64(t *testing.T) {
+	b := make([]byte, 8)
+
+	PutInt64(b, i64)
+	assert.Equal(t, i64, Int64(b))
+}
+
+func TestFloat32(t *testing.T) {
+	v := float32(1)
+	b := make([]byte, 4)
+
+	PutFloat32(b, v)
+	assert.Equal(t, v, Float32(b))
+}
+
+func TestFloat64(t *testing.T) {
+	v := float64(1)
+	b := make([]byte, 8)
+
+	PutFloat64(b, v)
+	assert.Equal(t, v, Float64(b))
+}
 
 func TestUint32LswFirst(t *testing.T) {
-	expect := uint32(0x03040102)
-	out := uint32(Uint32LswFirst([]byte{1, 2, 3, 4}))
-	if out != expect {
-		t.Errorf("wanted: %08x, got %08x", expect, out)
-	}
+	b := make([]byte, 4)
+
+	PutUint32LswFirst(b, u32)
+	assert.Equal(t, u32, Uint32LswFirst(b))
+}
+
+func TestInt32LswFirst(t *testing.T) {
+	b := make([]byte, 4)
+
+	PutInt32LswFirst(b, i32)
+	assert.Equal(t, i32, Int32LswFirst(b))
+}
+
+func TestFloat32LswFirst(t *testing.T) {
+	v := float32(1)
+	b := make([]byte, 4)
+
+	PutFloat32LswFirst(b, v)
+	assert.Equal(t, v, Float32LswFirst(b))
+}
+
+func TestUint64LswFirst(t *testing.T) {
+	v := uint64(1)
+	b := make([]byte, 8)
+
+	PutUint64LswFirst(b, v)
+	assert.Equal(t, v, Uint64LswFirst(b))
+}
+
+func TestInt64LswFirst(t *testing.T) {
+	v := int64(1)
+	b := make([]byte, 8)
+
+	PutInt64LswFirst(b, v)
+	assert.Equal(t, v, Int64LswFirst(b))
+}
+
+func TestFloat64LswFirst(t *testing.T) {
+	v := float64(1)
+	b := make([]byte, 8)
+
+	PutFloat64LswFirst(b, v)
+	assert.Equal(t, v, Float64LswFirst(b))
 }
