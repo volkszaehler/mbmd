6fbadb6842534e43d47406a860096b14fd59f656
Cyrill Schumacher <cyrill@schumacher.fm>
2024-03-24 10:39:09 +0100

    Fix websocket path

--- assets/js/app.js
+++ assets/js/app.js
@@ -130,9 +130,9 @@ function processMessage(data) {
 
 function connectSocket() {
 	var ws, loc = window.location;
-	var protocol = loc.protocol == "https:" ? "wss:" : "ws:"
+	var protocol = loc.protocol === "https:" ? "wss:" : "ws:"
 
-	ws = new WebSocket(protocol + "//" + loc.hostname + (loc.port ? ":" + loc.port : "") + loc.pathname.replace(/\/$/, "") + "/ws");
+	ws = new WebSocket(protocol + "//" + loc.hostname + (loc.port ? ":" + loc.port : "") + loc.pathname.replace(/\/$/, "") + "ws");
 
 	ws.onerror = function(evt) {
 		ws.close();
