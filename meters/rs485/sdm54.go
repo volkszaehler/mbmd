1da1793183981e3e102669c97aa15919166cc7a9
Matthias Koch <matthias.koch@algorithmic.de>
2024-11-29 20:58:45 +0100

    gofmt all files

--- meters/rs485/sdm54.go
+++ meters/rs485/sdm54.go
@@ -18,46 +18,46 @@ func NewSDM54Producer() Producer {
 	 * subtle differences (see 220, 230). Some opcodes might not work on some devices.
 	 */
 	ops := Opcodes{
-		VoltageL1:     0x0000, // 220, 230
-		VoltageL2:     0x0002,
-		VoltageL3:     0x0004,
-		CurrentL1:     0x0006, // 220, 230
-		CurrentL2:     0x0008,
-		CurrentL3:     0x000A,
-		PowerL1:       0x000C, //      230
-		PowerL2:       0x000E,
-		PowerL3:       0x0010,
+		VoltageL1:       0x0000, // 220, 230
+		VoltageL2:       0x0002,
+		VoltageL3:       0x0004,
+		CurrentL1:       0x0006, // 220, 230
+		CurrentL2:       0x0008,
+		CurrentL3:       0x000A,
+		PowerL1:         0x000C, //      230
+		PowerL2:         0x000E,
+		PowerL3:         0x0010,
 		ApparentPowerL1: 0x0012,
 		ApparentPowerL2: 0x0014,
 		ApparentPowerL3: 0x0016,
 		ReactivePowerL1: 0x0018,
 		ReactivePowerL2: 0x001A,
 		ReactivePowerL3: 0x001C,
-		CosphiL1:      0x001e, //      230
-		CosphiL2:      0x0020,
-		CosphiL3:      0x0022,
-		Power:         0x0034,
-		ApparentPower: 0x0038,
-		ReactivePower: 0x003C,
-		ImportPower:   0x0054,
-		ImportL1:      0x015a,
-		ImportL2:      0x015c,
-		ImportL3:      0x015e,
-		Import:        0x0048, // 220, 230
-		ExportL1:      0x0160,
-		ExportL2:      0x0162,
-		ExportL3:      0x0164,
-		Export:        0x004a, // 220, 230
-		SumL1:         0x0166,
-		SumL2:         0x0168,
-		SumL3:         0x016a,
-		Sum:           0x0156, // 220
-		Cosphi:        0x003e,
-		THDL1:         0x00ea, // voltage
-		THDL2:         0x00ec, // voltage
-		THDL3:         0x00ee, // voltage
-		THD:           0x00F8, // voltage
-		Frequency:     0x0046, //      230
+		CosphiL1:        0x001e, //      230
+		CosphiL2:        0x0020,
+		CosphiL3:        0x0022,
+		Power:           0x0034,
+		ApparentPower:   0x0038,
+		ReactivePower:   0x003C,
+		ImportPower:     0x0054,
+		ImportL1:        0x015a,
+		ImportL2:        0x015c,
+		ImportL3:        0x015e,
+		Import:          0x0048, // 220, 230
+		ExportL1:        0x0160,
+		ExportL2:        0x0162,
+		ExportL3:        0x0164,
+		Export:          0x004a, // 220, 230
+		SumL1:           0x0166,
+		SumL2:           0x0168,
+		SumL3:           0x016a,
+		Sum:             0x0156, // 220
+		Cosphi:          0x003e,
+		THDL1:           0x00ea, // voltage
+		THDL2:           0x00ec, // voltage
+		THDL3:           0x00ee, // voltage
+		THD:             0x00F8, // voltage
+		Frequency:       0x0046, //      230
 	}
 	return &SDM54Producer{Opcodes: ops}
 }
