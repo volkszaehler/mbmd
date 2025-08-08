499ae856f44e511b236660d9dce0021f758fc64e
guido4096 <guido@ngjansen.be>
2025-08-08 18:10:51 +0200

    Wago: fix power scaling (#368)

--- meters/rs485/wago87930.go
+++ meters/rs485/wago87930.go
@@ -27,18 +27,6 @@ func NewWago87930Producer() Producer {
 		CurrentL1:       0x500c,
 		CurrentL2:       0x500e,
 		CurrentL3:       0x5010,
-		Power:           0x5012,
-		PowerL1:         0x5014,
-		PowerL2:         0x5016,
-		PowerL3:         0x5018,
-		ReactivePower:   0x501a,
-		ReactivePowerL1: 0x501c,
-		ReactivePowerL2: 0x501e,
-		ReactivePowerL3: 0x5020,
-		ApparentPower:   0x5022,
-		ApparentPowerL1: 0x5024,
-		ApparentPowerL2: 0x5026,
-		ApparentPowerL3: 0x5028,
 		Import:          0x600c,
 		ImportL1:        0x6012,
 		ImportL2:        0x6014,
@@ -51,6 +39,18 @@ func NewWago87930Producer() Producer {
 		CosphiL2:        0x502e,
 		CosphiL3:        0x5030,
 		Cosphi:          0x502a,
+		Power:           0x5012,
+		PowerL1:         0x5014,
+		PowerL2:         0x5016,
+		PowerL3:         0x5018,
+		ReactivePower:   0x501a,
+		ReactivePowerL1: 0x501c,
+		ReactivePowerL2: 0x501e,
+		ReactivePowerL3: 0x5020,
+		ApparentPower:   0x5022,
+		ApparentPowerL1: 0x5024,
+		ApparentPowerL2: 0x5026,
+		ApparentPowerL3: 0x5028,
 	}
 	return &Wago87930Producer{Opcodes: ops}
 }
@@ -59,14 +59,19 @@ func (p *Wago87930Producer) Description() string {
 	return "Wago 879-30XX"
 }
 
-func (p *Wago87930Producer) snip(iec Measurement) Operation {
+func (p *Wago87930Producer) snip(iec Measurement, scaler ...float64) Operation {
+	transform := RTUIeee754ToFloat64 // default conversion
+	if len(scaler) > 0 {
+		transform = MakeScaledTransform(transform, scaler[0])
+	}
 	operation := Operation{
 		FuncCode:  ReadHoldingReg,
 		OpCode:    p.Opcode(iec),
 		ReadLen:   2,
 		IEC61850:  iec,
-		Transform: RTUIeee754ToFloat64,
+		Transform: transform,
 	}
+
 	return operation
 }
 
@@ -75,8 +80,17 @@ func (p *Wago87930Producer) Probe() Operation {
 }
 
 func (p *Wago87930Producer) Produce() (res []Operation) {
-	for op := range p.Opcodes {
+	for _, op := range []Measurement{
+		Voltage, VoltageL1, VoltageL2, VoltageL3, Frequency, Current, CurrentL1, CurrentL2, CurrentL3,
+		Import, ImportL1, ImportL2, ImportL3, Export, ExportL1, ExportL2, ExportL3, CosphiL1, CosphiL2, CosphiL3, Cosphi,
+	} {
 		res = append(res, p.snip(op))
+	}
+
+	for _, op := range []Measurement{
+		Power, PowerL1, PowerL2, PowerL3, ReactivePower, ReactivePowerL1, ReactivePowerL2, ReactivePowerL3, ApparentPower, ApparentPowerL1, ApparentPowerL2, ApparentPowerL3,
+	} {
+		res = append(res, p.snip(op, 0.001))
 	}
 
 	return res
