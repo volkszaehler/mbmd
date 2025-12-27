e3d89014e17647513f61b9c1097f830b9c184614
premultiply <4681172+premultiply@users.noreply.github.com>
2025-12-28 00:39:58 +0100

    fix ds100

--- meters/rs485/ds100.go
+++ meters/rs485/ds100.go
@@ -61,7 +61,7 @@ func (p *DS100Producer) Description() string {
 	return "B+G e-tech DS100"
 }
 
-func (p *DS100Producer) snip(iec Measurement, readlen uint16, sign signedness, transform RTUTransform, scaler ...float64) Operation {
+func (p *DS100Producer) snip(iec Measurement, readlen uint16, transform RTUTransform, scaler ...float64) Operation {
 	snip := Operation{
 		FuncCode:  ReadHoldingReg,
 		OpCode:    p.Opcodes[iec],
@@ -79,22 +79,22 @@ func (p *DS100Producer) snip(iec Measurement, readlen uint16, sign signedness, t
 
 // snip16u creates modbus operation for single register
 func (p *DS100Producer) snip16u(iec Measurement, scaler ...float64) Operation {
-	return p.snip(iec, 1, unsigned, RTUUint16ToFloat64, scaler...)
+	return p.snip(iec, 1, RTUUint16ToFloat64, scaler...)
 }
 
 // snip32u creates modbus operation for double register
 func (p *DS100Producer) snip32u(iec Measurement, scaler ...float64) Operation {
-	return p.snip(iec, 2, unsigned, RTUUint32ToFloat64, scaler...)
+	return p.snip(iec, 2, RTUUint32ToFloat64, scaler...)
 }
 
 // snip16s creates modbus operation for single register (signed)
 func (p *DS100Producer) snip16s(iec Measurement, scaler ...float64) Operation {
-	return p.snip(iec, 1, signed, RTUInt16ToFloat64, scaler...)
+	return p.snip(iec, 1, RTUInt16ToFloat64, scaler...)
 }
 
 // snip32s creates modbus operation for double register (signed)
 func (p *DS100Producer) snip32s(iec Measurement, scaler ...float64) Operation {
-	return p.snip(iec, 2, signed, RTUInt32ToFloat64, scaler...)
+	return p.snip(iec, 2, RTUInt32ToFloat64, scaler...)
 }
 
 func (p *DS100Producer) Probe() Operation {
