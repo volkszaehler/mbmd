6cbf4285cea8ca4e53f2510d0abb8cf70e7bc685
premultiply <4681172+premultiply@users.noreply.github.com>
2026-01-07 08:45:46 +0100

    Eastron: enhance, unify and fix (#377)

--- meters/rs485/sdm.go
+++ meters/rs485/sdm.go
@@ -18,45 +18,58 @@ func NewSDMProducer() Producer {
 	 * subtle differences (see 220, 230). Some opcodes might not work on some devices.
 	 */
 	ops := Opcodes{
-		VoltageL1:     0x0000, // 220, 230
-		VoltageL2:     0x0002,
-		VoltageL3:     0x0004,
-		CurrentL1:     0x0006, // 220, 230
-		CurrentL2:     0x0008,
-		CurrentL3:     0x000A,
-		PowerL1:       0x000C, //      230
-		PowerL2:       0x000E,
-		PowerL3:       0x0010,
-		Power:         0x0034,
-		ApparentPower: 0x0038,
-		ReactivePower: 0x003C,
-		ImportPower:   0x0054,
-		ImportL1:      0x015a,
-		ImportL2:      0x015c,
-		ImportL3:      0x015e,
-		Import:        0x0048, // 220, 230
-		ExportL1:      0x0160,
-		ExportL2:      0x0162,
-		ExportL3:      0x0164,
-		Export:        0x004a, // 220, 230
-		SumL1:         0x0166,
-		SumL2:         0x0168,
-		SumL3:         0x016a,
-		Sum:           0x0156, // 220
-		CosphiL1:      0x001e, //      230
-		CosphiL2:      0x0020,
-		CosphiL3:      0x0022,
-		Cosphi:        0x003e,
-		THDL1:         0x00ea, // voltage
-		THDL2:         0x00ec, // voltage
-		THDL3:         0x00ee, // voltage
-		THD:           0x00F8, // voltage
-		Frequency:     0x0046, //      230
-		//L1THDCurrent: 0x00F0, // current
-		//L2THDCurrent: 0x00F2, // current
-		//L3THDCurrent: 0x00F4, // current
-		//AvgTHDCurrent: 0x00Fa, // current
-		//ApparentImportPower: 0x0064,
+		VoltageL1:        0x0000, // Phase 1 line to neutral volts
+		VoltageL2:        0x0002, // Phase 2 line to neutral volts
+		VoltageL3:        0x0004, // Phase 3 line to neutral volts
+		CurrentL1:        0x0006, // Phase 1 current
+		CurrentL2:        0x0008, // Phase 2 current
+		CurrentL3:        0x000A, // Phase 3 current
+		PowerL1:          0x000C, // Phase 1 active power
+		PowerL2:          0x000E, // Phase 2 active power
+		PowerL3:          0x0010, // Phase 3 active power
+		ApparentPowerL1:  0x0012, // Phase 1 apparent power
+		ApparentPowerL2:  0x0014, // Phase 2 apparent power
+		ApparentPowerL3:  0x0016, // Phase 3 apparent power
+		ReactivePowerL1:  0x0018, // Phase 1 reactive power
+		ReactivePowerL2:  0x001A, // Phase 2 reactive power
+		ReactivePowerL3:  0x001C, // Phase 3 reactive power
+		CosphiL1:         0x001E, // Phase 1 power factor
+		CosphiL2:         0x0020, // Phase 2 power factor
+		CosphiL3:         0x0022, // Phase 3 power factor
+		Power:            0x0034, // Total system power
+		ApparentPower:    0x0038, // Total system volt amps.
+		ReactivePower:    0x003C, // Total system VAr
+		Cosphi:           0x003E, // Total system power factor
+		PhaseAngle:       0x0042, // Total system phase angle
+		Frequency:        0x0046, // Frequency of supply voltages
+		Import:           0x0048, // Total Import kWh
+		Export:           0x004A, // Total Export kWh
+		ReactiveImport:   0x004C, // Total Import kVArh
+		ReactiveExport:   0x004E, // Total Export kVArh
+		THDL1:            0x00EA, // Phase 1 L/N volts THD
+		THDL2:            0x00EC, // Phase 2 L/N volts THD
+		THDL3:            0x00EE, // Phase 3 L/N volts THD
+		THD:              0x00F8, // Average line to neutral volts THD
+		Sum:              0x0156, // Total kWh
+		ReactiveSum:      0x0158, // Total kVArh
+		ImportL1:         0x015A, // L1 import kWh
+		ImportL2:         0x015C, // L2 import kWh
+		ImportL3:         0x015E, // L3 import kWh
+		ExportL1:         0x0160, // L1 export kWh
+		ExportL2:         0x0162, // L2 export kWh
+		ExportL3:         0x0164, // L3 export kWh
+		SumL1:            0x0166, // L1 total kWh
+		SumL2:            0x0168, // L2 total kWh
+		SumL3:            0x016A, // L3 total kWh
+		ReactiveImportL1: 0x016C, // L1 import kVArh
+		ReactiveImportL2: 0x016E, // L2 import kVArh
+		ReactiveImportL3: 0x0170, // L3 import kVArh
+		ReactiveExportL1: 0x0172, // L1 export kVArh
+		ReactiveExportL2: 0x0174, // L2 export kVArh
+		ReactiveExportL3: 0x0176, // L3 export kVArh
+		ReactiveSumL1:    0x0178, // L1 total kVArh
+		ReactiveSumL2:    0x017A, // L2 total kVArh
+		ReactiveSumL3:    0x017C, // L3 total kVArh
 	}
 	return &SDMProducer{Opcodes: ops}
 }
--- meters/rs485/sdm54.go
+++ meters/rs485/sdm54.go
@@ -14,50 +14,61 @@ func NewSDM54Producer() Producer {
 	/**
 	 * Opcodes as defined by Eastron SDM54.
 	 * See https://www.eastrongroup.com/eastrongroup/2024/08/21/eastronsdm54seriesusermanualv1.2.pdf
-	 * This is to a large extent a superset of all SDM devices, however there are
-	 * subtle differences (see 220, 230). Some opcodes might not work on some devices.
 	 */
 	ops := Opcodes{
-		VoltageL1:       0x0000, // 220, 230
-		VoltageL2:       0x0002,
-		VoltageL3:       0x0004,
-		CurrentL1:       0x0006, // 220, 230
-		CurrentL2:       0x0008,
-		CurrentL3:       0x000A,
-		PowerL1:         0x000C, //      230
-		PowerL2:         0x000E,
-		PowerL3:         0x0010,
-		ApparentPowerL1: 0x0012,
-		ApparentPowerL2: 0x0014,
-		ApparentPowerL3: 0x0016,
-		ReactivePowerL1: 0x0018,
-		ReactivePowerL2: 0x001A,
-		ReactivePowerL3: 0x001C,
-		CosphiL1:        0x001e, //      230
-		CosphiL2:        0x0020,
-		CosphiL3:        0x0022,
-		Power:           0x0034,
-		ApparentPower:   0x0038,
-		ReactivePower:   0x003C,
-		ImportPower:     0x0054,
-		ImportL1:        0x015a,
-		ImportL2:        0x015c,
-		ImportL3:        0x015e,
-		Import:          0x0048, // 220, 230
-		ExportL1:        0x0160,
-		ExportL2:        0x0162,
-		ExportL3:        0x0164,
-		Export:          0x004a, // 220, 230
-		SumL1:           0x0166,
-		SumL2:           0x0168,
-		SumL3:           0x016a,
-		Sum:             0x0156, // 220
-		Cosphi:          0x003e,
-		THDL1:           0x00ea, // voltage
-		THDL2:           0x00ec, // voltage
-		THDL3:           0x00ee, // voltage
-		THD:             0x00F8, // voltage
-		Frequency:       0x0046, //      230
+		VoltageL1:        0x0000, // Phase 1 line to neutral volts
+		VoltageL2:        0x0002, // Phase 2 line to neutral volts
+		VoltageL3:        0x0004, // Phase 3 line to neutral volts
+		CurrentL1:        0x0006, // Phase 1 current
+		CurrentL2:        0x0008, // Phase 2 current
+		CurrentL3:        0x000A, // Phase 3 current
+		PowerL1:          0x000C, // Phase 1 active power
+		PowerL2:          0x000E, // Phase 2 active power
+		PowerL3:          0x0010, // Phase 3 active power
+		ApparentPowerL1:  0x0012, // Phase 1 apparent power
+		ApparentPowerL2:  0x0014, // Phase 2 apparent power
+		ApparentPowerL3:  0x0016, // Phase 3 apparent power
+		ReactivePowerL1:  0x0018, // Phase 1 reactive power
+		ReactivePowerL2:  0x001A, // Phase 2 reactive power
+		ReactivePowerL3:  0x001C, // Phase 3 reactive power
+		CosphiL1:         0x001E, // Phase 1 power factor
+		CosphiL2:         0x0020, // Phase 2 power factor
+		CosphiL3:         0x0022, // Phase 3 power factor
+		Power:            0x0034, // Total system power
+		ApparentPower:    0x0038, // Total system volt amps.
+		ReactivePower:    0x003C, // Total system VAr
+		Cosphi:           0x003E, // Total system power factor
+		PhaseAngle:       0x0042, // Total system phase angle
+		Frequency:        0x0046, // Frequency of supply voltages
+		Import:           0x0048, // Total Import kWh
+		Export:           0x004A, // Total Export kWh
+		THDL1:            0x00EA, // Phase 1 L/N volts THD
+		THDL2:            0x00EC, // Phase 2 L/N volts THD
+		THDL3:            0x00EE, // Phase 3 L/N volts THD
+		THD:              0x00F8, // Average line to neutral volts THD
+		Sum:              0x0156, // Total kWh
+		ReactiveSum:      0x0158, // Total kVArh
+		ImportL1:         0x015A, // L1 import kWh
+		ImportL2:         0x015C, // L2 import kWh
+		ImportL3:         0x015E, // L3 import kWh
+		ExportL1:         0x0160, // L1 export kWh
+		ExportL2:         0x0162, // L2 export kWh
+		ExportL3:         0x0164, // L3 export kWh
+		SumL1:            0x0166, // L1 total kWh
+		SumL2:            0x0168, // L2 total kWh
+		SumL3:            0x016A, // L3 total kWh
+		SumT1:            0x130C, // Tariff 1 total kWh
+		SumT2:            0x130E, // Tariff 2 total kWh
+		ImportT1:         0x1314, // Tariff 1 import kWh
+		ImportT2:         0x1316, // Tariff 2 import kWh
+		ExportT1:         0x131C, // Tariff 1 export kWh
+		ExportT2:         0x131E, // Tariff 2 export kWh
+		ReactiveSumT1:    0x1324, // Tariff 1 total kVArh
+		ReactiveSumT2:    0x1326, // Tariff 2 total kVArh
+		ReactiveImportT1: 0x132C, // Tariff 1 import kVArh
+		ReactiveImportT2: 0x132E, // Tariff 2 import kVArh
+		ReactiveExportT1: 0x1334, // Tariff 1 export kVArh
+		ReactiveExportT2: 0x1336, // Tariff 2 export kVArh
 	}
 	return &SDM54Producer{Opcodes: ops}
 }
